3f40bc6c4f8030f552b42fce1e239e49be084a2e
khanza2003 <knzkenu@gmail.com>
2024-12-21 17:08:51 +0530

    initial v6

--- routes/router.js
+++ routes/router.js
@@ -1,36 +1,4 @@
-// const express = require('express');
-// const userController = require('../controllers/userController');
-// const researchController = require('../controllers/researchController');
-// const jwtMiddleware = require('../middlewares/jwtMiddleware');
-// const multerMiddleware = require('../middlewares/multerMiddleware');
 
-// const router = new express.Router();
-
-// // Register - POST
-// router.post('/register', userController.registerController);
-
-// // Login - POST
-// router.post('/login', userController.loginController);
-
-// // Add Research - POST
-// router.post('/add-research', jwtMiddleware, multerMiddleware.single('file'), researchController.addResearchController);
-
-// // Get User Research - GET
-// router.get('/user-research', jwtMiddleware, researchController.getUserResearchController);
-
-// // Get All Research - GET
-// router.get('/all-research', jwtMiddleware, researchController.getAllResearchController);
-
-// // Edit Research - PUT
-// router.put('/research/:id/edit', jwtMiddleware, multerMiddleware.single("file"), researchController.editResearchController);
-
-// // Remove Research - DELETE
-// router.delete('/research/:id/remove', jwtMiddleware, researchController.removeResearchController);
-
-// // Edit User - PUT
-// router.put('/user/edit', jwtMiddleware, multerMiddleware.single("profilePic"), userController.editUserController);
-
-// module.exports = router;
 const express = require('express');
 const userController = require('../controllers/userController');
 const researchController = require('../controllers/researchController');
